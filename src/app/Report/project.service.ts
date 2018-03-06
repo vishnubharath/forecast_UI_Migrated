284e288ad45509c464bc2738d64b3eb41e7255af
273840 <273840@PC312610.cts.com>
2018-03-06 14:38:58 +0530

    Project Filter

---
+++ src/app/Report/project.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable }     from 'rxjs/Observable';
+import { Constants } from '../constants';
+import { Report } from './report';
+import { error } from 'util';
+import {Project } from './Project'
+
+@Injectable()
+export class ProjectService{
+
+	constructor(private _http:Http){
+
+	}
+
+	getAllProjects():Observable<Project[]>{
+		return this._http
+		.get(Constants.base_url+'projects/all' )
+		.map((res:Response) => res.json())
+		.catch(error=>{
+			let errMsg = (error.message) ? error.message :
+				error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+			console.error(errMsg); // log to console instead
+			return Observable.throw(errMsg);
+		});
+	}
+
+}
